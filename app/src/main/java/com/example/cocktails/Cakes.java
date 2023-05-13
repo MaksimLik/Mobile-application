1a1822ae5af1c9834c926413aebb8d237f81884b
Maksym <72620745+MaksimLik@users.noreply.github.com>
2023-05-13 18:43:29 +0300

    Add information about Cakes

--- app/src/main/java/com/example/cocktails/Cakes.java
+++ app/src/main/java/com/example/cocktails/Cakes.java
@@ -6,45 +6,106 @@ public class Cakes {
     private String recipe;
 
     private int imageResourceId;
-
-//    public Cocktail(String name, String recipe) {
-//        this.name = name;
-//        this.recipe = recipe;
-//    }
-//
-//    public static final Cocktail[] cocktails = {
-//            new Cocktail("Bloody Mary", "Składniki:\n 40 ml wódki\n 10 ml soku z cytryny\n 120 ml soku pomidorowego\n sos worchestershire\n sól " +
-//                    "\n pieprz\n tabasco\n gałązka selera naciowego \n" +
-//                    "\n Sposób przygotowania: " +
-//                    "\n Wszystkie składniki wymieszać w szklance z lodem i ozdobić selerem naciowym."),
-//
-//            new Cocktail("Red Beach", "Składnik:\n 122 ml piwa\n 10 ml wody z cytryny\n 120 ml wódki pomidorowej\n sos worchestershire\n cukier " +
-//                    "\n Sposób przygotowania: " +
-//                    "\n Wszystkie składniki wymieszać w sklance z lodem i ozdobić selerem naciowym.")
-//
-//    };
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getRecipe() {
-//        return recipe;
-//    }
-//
-//    public void setRecipe(String recipe) {
-//        this.recipe = recipe;
-//    }
+    
 
     public static final Cakes[] cocktails = {
-            new Cakes("Cocktail #111", R.drawable.image01, "11111"),
-            new Cakes("Cocktail #222", R.drawable.image02, "2222"),
-            new Cakes("Cocktail #333", R.drawable.image03, "333"),
-            new Cakes("Cocktail #444", R.drawable.image04,"444" )};
+            new Cakes("Cocoa-cupcakes", R.drawable.cake1, "Wheat flour - 120 g\n" +
+                    "\n" +
+                    "Milk - 80 ml\n" +
+                    "\n" +
+                    "Butter - 75 g\n" +
+                    "\n" +
+                    "Sugar - 75 g\n" +
+                    "\n" +
+                    "Cocoa powder - 2.5 tbsp.l.\n" +
+                    "\n" +
+                    "Chicken egg - 1 pc.\n" +
+                    "\n" +
+                    "Baking powder - 1 tsp.\n" +
+                    "Combine milk and butter in a small saucepan / saucepan, put on fire and warm up until the butter dissolves. Then add sugar and cocoa powder.\n" +
+                    "Mix well and bring the mixture to a boil, but do not boil. The dough consistency should resemble sour cream of medium density. Bake chocolate cupcakes with cocoa in silicone molds for 20-25 minutes."),
+            new Cakes("Standard cupcake", R.drawable.cake2, "120 g butter;\n" +
+                    "130 g of sugar;\n" +
+                    "vanilla sugar — to taste;\n" +
+                    "3 eggs;\n" +
+                    "200 g flour;\n" +
+                    "1½ teaspoons baking powder;\n" +
+                    "1 1/2 teaspoons salt;\n" +
+                    "60 ml of milk.\n" +
+                    "Using a mixer, beat the butter with plain and vanilla sugar until fluffy. Enter the eggs one at a time, beating for a few minutes after each addition.\n" +
+                    "\n" +
+                    "Separately combine flour, baking powder and salt. Continuing to beat, pour half of the flour mixture into the egg mixture. Then add the milk and the remaining flour. Do not knead the dough for a long time, otherwise the butter will start to melt."),
+            new Cakes("Cupcake with cream", R.drawable.cake3, "120 g butter;\n" +
+                    "130 g of sugar;\n" +
+                    "vanilla sugar — to taste;\n" +
+                    "3 eggs;\n" +
+                    "200 g flour;\n" +
+                    "1½ teaspoons baking powder;\n" +
+                    "1 1/2 teaspoons salt;\n" +
+                    "60 ml of milk.\n" +
+                    "For cream and decoration:\n" +
+                    "\n" +
+                    "200 g of cottage cheese;\n" +
+                    "20 g of cream with a fat content of 33%;\n" +
+                    "20 g powdered sugar;\n" +
+                    "any berries — to taste.\n" +
+                    "All ingredients for cupcakes should be at room temperature.\n" +
+                    "\n" +
+                    "Using a mixer, beat the butter with plain and vanilla sugar until fluffy. Enter the eggs one at a time, beating for a few minutes after each addition.\n" +
+                    "\n" +
+                    "Separately combine flour, baking powder and salt. Continuing to beat, pour half of the flour mixture into the egg mixture. Then add the milk and the remaining flour. Do not knead the dough for a long time, otherwise the butter will start to melt."),
+            new Cakes("Cottage cheese cupcakes", R.drawable.cake4,"1 orange;\n" +
+                    "10 g baking powder;\n" +
+                    "a pinch of salt;\n" +
+                    "a pinch of soda;\n" +
+                    "200 g of cottage cheese;\n" +
+                    "3 eggs;\n" +
+                    "2 cups of sugar (200 ml);\n" +
+                    "vanilla sugar — to taste;\n" +
+                    "100 g butter;\n" +
+                    "150 ml of milk;\n" +
+                    "3 cups of flour (200 ml).\n" +
+                    "Grate the orange zest on a fine grater. Put all the ingredients, except flour, in a bowl. Beat with a mixer until smooth. Then enter the flour in parts, carefully kneading the dough.\n" +
+                    "\n" +
+                    "Arrange the dough into cupcake molds. Place in a preheated 180°C oven for about 30 minutes."),
+            new Cakes("Cupcake", R.drawable.cake5,"Ingredients for cupcakes:\n" +
+                    "2.5 cups flour;\n" +
+                    "2 tbsp cocoa;\n" +
+                    "1 tsp salt;\n" +
+                    "1.5 cups of sugar;\n" +
+                    "2 eggs;\n" +
+                    "60 g butter;\n" +
+                    "½ tsp red gel dye;\n" +
+                    "1 tsp vanilla extract;\n" +
+                    "1 cup kefir;\n" +
+                    "1.5 tsp soda;\n" +
+                    "2 tsp vinegar\n" +
+                    "For the cream:\n" +
+                    "225 g butter;\n" +
+                    "340 g cream cheese;\n" +
+                    "450 g of sifted powdered sugar;\n" +
+                    "3/4 teaspoons vanilla extract.\n" +
+                    "Preheat the oven to 180 degrees, lay out metal cupcake molds and line paper molds in each hole.\n" +
+                    "Mix flour, cocoa and salt.\n" +
+                    "In a separate bowl, beat the butter and sugar with a mixer, add the eggs one at a time and continue to beat. Pour in the food coloring and vanilla.\n" +
+                    "Slow down the speed of the mixer, pour the flour mixture in three stages, alternating with the addition of kefir. Continue whisking at low speed.\n" +
+                    "In a separate bowl, extinguish the soda with vinegar, add the mixture to the dough and beat with a mixer at medium speed for about 10 seconds.\n" +
+                    "Fill the paper molds with the dough for ¾ and bake in the oven for about 20 minutes. Cool the finished cupcakes on the grill.\n" +
+                    "Whisk all the ingredients for the glaze, send it to a pastry bag and squeeze it onto the cupcakes. The glaze can also be spread with a spoon."),
+            new Cakes("Cupcake", R.drawable.cake6,"egg - 3 pcs.\n" +
+                    "sugar - 200g\n" +
+                    "milk - 1 cup\n" +
+                    "vegetable oil - 1/2 cup\n" +
+                    "butter - 1 tbsp\n" +
+                    "vanilla essence\n" +
+                    "flour - 2 cups\n" +
+                    "cocoa powder - 2 tbsp\n" +
+                    "baking powder - 10g\n" +
+                    "To prepare a stove-top cupcake without baking, mix 1 cup of sugar with eggs until white, then add vanilla essence or vanilla sugar, half a cup of vegetable oil, and 1 cup of milk. Mix in sifted flour and baking powder. " +
+                    "Separate half of the dough and add 2 tablespoons of cocoa and 1 tablespoon of milk to the second half.\n" +
+                    "Grease and flour a thick-walled pan and place a heavy glass in the center. \n" +
+                    "Pour the dough around the glass, alternating the colors. Cook on medium heat for 5 minutes and then reduce to minimum heat for 40 minutes. Check the cupcake's readiness with a toothpick.\n" +
+                    "Remove from heat, let it cool completely, and decorate if desired.")};
 
     private Cakes(String name, int imageResourceId, String recipe) {
         this.name = name;
